6bab9b0e32c8b25a26981efdf523919d74ed5bfe
StackZ <47382115+SuperSaiyajinStackZ@users.noreply.github.com>
2020-04-05 19:51:02 +0200

    Update Credits.

--- source/screens/credits.cpp
+++ source/screens/credits.cpp
@@ -59,8 +59,8 @@ void Credits::Draw(void) const {
 
 	if (creditsPage == 1) {
 		Gui::DrawStringCentered(0, -2, 0.7f, Config::TxtColor, Lang::get("TRANSLATORS"), 320);
-		Gui::DrawString(5, 30, 0.6f, Config::TxtColor, "- _mapple²\n- antoine62\n- Chips\n- David Pires\n- FlameKat53\n- lemonnade0\n- Pk11\n- Roby Spia\n- StackZ\n- YoSoy");
-		Gui::DrawString(180, 30, 0.6f, Config::TxtColor, "Русский\nFrançais\nPortuguês\nPortuguês\nBruh\nLietuvių\n日本語\nItaliano\nDeutsch, English\nEspañol");
+		Gui::DrawString(5, 21, 0.6f, Config::TxtColor, "- _mapple²\n- antoine62\n- Chips\n- cooolgamer\n- David Pires\n- FlameKat53\n- lemonnade0\n- Pk11\n- Roby Spia\n- StackZ\n- YoSoy");
+		Gui::DrawString(180, 21, 0.6f, Config::TxtColor, "Русский\nFrançais\nPortuguês\nFrançais\nPortuguês\nBruh\nLietuvių\n日本語\nItaliano\nDeutsch, English\nEspañol");
 	} else if (creditsPage == 2) {
 		Gui::DrawStringCentered(0, -2, 0.7f, Config::TxtColor, "Universal-Team", 320);
 		Gui::DrawStringCentered(0, 35, 0.7f, Config::TxtColor, "DeadPhoenix");
