b1796023ab19f5cf5e051d29005c976b37111543
Pk11 <epicpkmn11@outlook.com>
2021-07-21 01:08:44 -0500

    Add "make citra" Makefile rule

--- source/utils/download.cpp
+++ source/utils/download.cpp
@@ -439,7 +439,11 @@ Result downloadFromRelease(const std::string &url, const std::string &asset, con
 	@return True if Wi-Fi is connected; false if not.
 */
 bool checkWifiStatus(void) {
-	return true; // For citra.
+#ifdef CITRA
+	// Citra's Wi-Fi check doesn't work
+	return true;
+#endif
+
 	u32 wifiStatus;
 	bool res = false;
 
