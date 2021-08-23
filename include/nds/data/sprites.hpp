974a92840e3a5d278b8df8d29c07b452b7921fce
Pk11 <epicpkmn11@outlook.com>
2021-08-22 23:34:38 -0500

    Add a few DS images

---
+++ include/nds/data/sprites.hpp
@@ -0,0 +1,12 @@
+#ifndef _UNIVERSAL_UPDATER_SPRITES_HPP
+#define _UNIVERSAL_UPDATER_SPRITES_HPP
+
+#define sprites_download_idx 0
+#define sprites_info_idx 1
+#define sprites_noIconinfo_idx 2
+#define sprites_queue0_idx 3
+#define sprites_search_idx 4
+#define sprites_settings_idx 5
+#define sprites_sort_idx 6
+
+#endif
