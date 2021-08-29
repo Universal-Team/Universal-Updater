12d01407b4de76dc85f6ae944e757de80763b460
Pk11 <epicpkmn11@outlook.com>
2021-08-28 19:01:42 -0500

    Add dsiwifi, nds-mbedtls, and nds-curl libs

---
+++ nds/libs/include/dswifi_version.h
@@ -0,0 +1,10 @@
+#ifndef _dswifi_version_h_
+#define _dswifi_version_h_
+
+#define DSIWIFI_MAJOR    0
+#define DSIWIFI_MINOR    1
+#define DSIWIFI_REVISION 0
+
+#define DSIWIFI_VERSION "0.1.0"
+
+#endif // _dsiwifi_version_h_
