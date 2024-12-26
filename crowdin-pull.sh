6fb3d4620ae0b5afbcdbf01023962955bb513c9c
lifehackerhansol <lifehackerhansol@ds-homebrew.com>
2024-12-25 16:57:38 -0800

    workflow: add Crowdin automation scripts

---
+++ crowdin-pull.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Add new languages here, space separated and using the ID for `crowdin pull`
+LANGUAGES="bruh da de es-ES fr hu it ja ko lt nl no pl pt-PT pt-BR ro ru ry tr uk zh-CN zh-TW"
+
+ARG=''
+for LANGUAGE in $LANGUAGES; do
+	ARG+="-l $LANGUAGE "
+done
+crowdin pull $ARG
